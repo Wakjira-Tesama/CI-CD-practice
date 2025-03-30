3b1e46536940366d47472ab846bb2ff7ff024d2c
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-03-30 10:15:34 +0300

    new test

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from  main import solution
 def two_sum_test():
     assert solution([1, 9, 3, 4, 5], 5) == [0, 3], "Test case failed"
     print("Test case passed")
-
+    assert True == False
 two_sum_test()
